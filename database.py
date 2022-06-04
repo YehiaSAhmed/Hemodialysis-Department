ec02ccb344453770f43a92bdd97c4780c935b800
sohailamahmoud <81927516+sohailamahmoud@users.noreply.github.com>
2022-06-05 00:27:44 +0200

    Update database.py

--- database.py
+++ database.py
@@ -14,3 +14,38 @@ mycursor.execute("SHOW TABLES")
 for x in mycursor:
   print(x) 
 
+mycursor.execute("SHOW COLUMNS FROM admin")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM care")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM doctor")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM medical_history")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM nurse")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM patient")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM prescribe")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM prescription")
+for x in mycursor:
+  print(x) 
+
+mycursor.execute("SHOW COLUMNS FROM treat")
+for x in mycursor:
+  print(x) 
